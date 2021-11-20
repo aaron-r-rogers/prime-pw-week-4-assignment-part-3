a43bf12563d4a4e917322458cdf6675771e6a613
aaron-r-rogers <rogers.aaron.r@gmail.com>
2021-11-20 10:08:12 -0600

    All tasks complete; working on stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,41 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = ['banana', 'orange'];
+const maxItems = 5
+
+function addItem(item, basket) {
+    basket.push(item);
+    return true;
+}
+
+function listItems(basket) {
+    for (items of basket) {
+        console.log(items);
+    }
+    return 'list'
+}
+
+/*
+function listItems(basket) {
+    for (let i = 0; i < basket.length; i++) {
+        console.log(basket[i]);
+    }
+    return true
+}
+*/
+
+function empty(basket) {
+    basket.splice(0, basket.length)
+    return 'emptied'
+}
+
+function isFull()
+
+console.log('startTest');
+console.log(addItem('apples', basket));
+console.log(listItems(basket));
+console.log(basket);
+console.log(empty(basket));
+console.log(basket);
+console.log('stopTest');
